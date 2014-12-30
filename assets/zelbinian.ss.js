a9ab7673e383f7e008dd6de60d89f4373165d49d
Zelbinian <dustin.l.hodge@gmail.com>
2014-12-29 20:18:21 -0800

    It lives!
    Wiring up the javascript to the HTML with jQuery for the first time.
    This actually almost counts as a minimum viable product, as is.

--- assets/zelbinian.ss.js
+++ assets/zelbinian.ss.js
@@ -1,9 +1,21 @@
+/** FUNCTIONS **/
+
 /**
  * Generates the information necessary for the website to decide what outfit to suggest
  */
 function randomStyle() {
 
     // the basis for deciding on an outfit
-    return Math.floor(Math.random() % 20) + 1;
+    return (Math.floor(Math.random() * 20) + 1);
 
 }
+
+/** SCRIPTS **/
+
+/**
+ * When the document is ready, write the result of the randomStyle function to the h1
+ * tag with the "target" id.
+ */
+$(document).ready(function () {
+    $("#target").html(randomStyle);
+});
