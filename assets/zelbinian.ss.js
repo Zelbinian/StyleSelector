03875d1bbdc2ab3f135777d3fce4a61523ad13b6
Zelbinian <dustin.l.hodge@gmail.com>
2015-01-01 11:02:20 -0800

    Making a variable
    First step in making this more complicated is to capture the random roll
    in a variable instead of just directly returning it

--- assets/zelbinian.ss.js
+++ assets/zelbinian.ss.js
@@ -4,9 +4,11 @@
  * Generates the information necessary for the website to decide what outfit to suggest
  */
 function randomStyle() {
-
-    // the basis for deciding on an outfit
-    return (Math.floor(Math.random() * 20) + 1);
+    
+    // the basis for deciding on an outfit, which is based on me originally rolling a d20
+    var choice = Math.floor(Math.random() * 20) + 1;
+    
+    return (choice);
 
 }
 
