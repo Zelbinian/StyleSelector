231dd585eae3265c2d2f21134b8ac99630aa6d44
Dustin Hodge <dlhodge@uw.edu>
2015-01-21 14:23:37 -0800

    Tests for randomNumber function
    Wrote a few tests; more could be written but this is a
    specifically-written helper function so it shouldn't be a big deal.

--- test/zelbinian.ss.test.js
+++ test/zelbinian.ss.test.js
@@ -1,3 +1,22 @@
-QUnit.test("randomNumber Same Value Tests", function( assert ) {
+QUnit.module("randomNumber tests");
+QUnit.test("randomNumber two zeroes", function( assert ) {
     assert.deepEqual(randomNumber(0,0), 0, "When two zeroes are entered, should return 0");
+});
+
+QUnit.test("randomNumber two same value positive integers", function( assert ) {           
+    assert.deepEqual(randomNumber(10,10), 10, "When two of the same integer are entered, should return that same integer");
+});
+
+QUnit.test("randomNumber two same value negative integers", function( assert ) {           
+    assert.deepEqual(randomNumber(-1,-1), -1, "When two negative integers are entered, should return that same integer");
+});
+
+QUnit.test("randomNumber stays in range", function( assert ) {
+    var result = randomNumber(1,10);
+    assert.ok((result >= 1 && result <= 10), "Expected result to be between 1 and 10 and got " + result); 
+});
+
+QUnit.test("randomNumber flipped inputs", function( assert ) {
+    var result = randomNumber(10,1);
+    assert.ok((result >= 1 && result <= 10), "Expected result to be between 1 and 10 and got " + result); 
 });
