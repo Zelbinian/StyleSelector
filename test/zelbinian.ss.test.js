d1ea59c3371b35bfbfffbef87121f2fd51a57dc5
Dustin Hodge <dlhodge@uw.edu>
2015-01-22 10:20:58 -0800

    Refactoring tests
    The tests were a little too atomic.

--- test/zelbinian.ss.test.js
+++ test/zelbinian.ss.test.js
@@ -1,13 +1,7 @@
 QUnit.module("randomNumber tests");
-QUnit.test("randomNumber two zeroes", function( assert ) {
+QUnit.test("randomNumber equal number tests", function( assert ) {
     assert.deepEqual(randomNumber(0,0), 0, "When two zeroes are entered, should return 0");
-});
-
-QUnit.test("randomNumber two same value positive integers", function( assert ) {           
     assert.deepEqual(randomNumber(10,10), 10, "When two of the same integer are entered, should return that same integer");
-});
-
-QUnit.test("randomNumber two same value negative integers", function( assert ) {           
     assert.deepEqual(randomNumber(-1,-1), -1, "When two negative integers are entered, should return that same integer");
 });
 
